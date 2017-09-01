5758e02b790b8e22a0ff82e2ffca47a5366e5b41
Kristian Lesko <kristian.lesko@gooddata.com>
2017-09-01 13:22:45 +0200

    Add requirements file
    For easier pulling of dependencies when running the tool
    during config PR testing, we need to define the tool dependencies
    in a separate file that we can reference later.

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (C) 2007-2012, GoodData(R) Corporation. All rights reserved
+# Copyright (C) 2007-2017, GoodData(R) Corporation. All rights reserved
 
 import os
 from setuptools import setup
+
+
+with open('requirements.txt') as deps_file:
+    deps = [i.replace('\n', '') for i in deps_file.readlines()]
 
 
 # Parameters for build
@@ -20,7 +24,7 @@ params = {
     'author_email': 'root@gooddata.com',
     'description': 'FreeIPA Manager',
     'long_description': 'GoodData FreeIPA management tooling',
-    'install_requires': ['pyyaml', 'voluptuous', 'yamllint']
+    'install_requires': deps
 }
 
 setup(**params)
