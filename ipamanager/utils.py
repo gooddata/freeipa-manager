0b709b6d126a65bcae95f64a2e8c840be9fd731c
Kristián Leško <kristian.lesko@gooddata.com>
2022-06-10 09:16:15 +0200

    Remove yamllint dependency from the repo

--- ipamanager/utils.py
+++ ipamanager/utils.py
@@ -18,11 +18,8 @@ import socket
 import sys
 import voluptuous
 import yaml
-from yamllint.config import YamlLintConfig
-from yamllint.linter import run as yamllint_check
 
 import entities
-from errors import ConfigError
 from schemas import schema_settings
 
 
@@ -189,20 +186,6 @@ def parse_args():
     return args
 
 
-def run_yamllint_check(data):
-    """
-    Run a yamllint check on parsed file contents
-    to verify that the file syntax is correct.
-    :param str data: contents of the configuration file to check
-    :param yamllint.config.YamlLintConfig: yamllint config to use
-    :raises ConfigError: in case of yamllint errors
-    """
-    rules = {'extends': 'default', 'rules': {'line-length': 'disable'}}
-    lint_errs = list(yamllint_check(data, YamlLintConfig(yaml.dump(rules))))
-    if lint_errs:
-        raise ConfigError('yamllint errors: %s' % lint_errs)
-
-
 def _merge_include(target, source):
     for key, value in source.iteritems():
         if isinstance(value, dict) and key in target:
@@ -223,7 +206,6 @@ def load_settings(path):
     result = {}
     with open(path) as src:
         raw = src.read()
-        run_yamllint_check(raw)
         settings = yaml.safe_load(raw)
     # run validation of parsed YAML against schema
     voluptuous.Schema(schema_settings)(settings)
